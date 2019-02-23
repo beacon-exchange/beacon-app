0d97d50fdf3abd40b76cd03068ed2a63ea95c765
Janning <thejanning@The.Purge>
2019-02-22 17:54:38 -0800

    Add Function handleLength()
    * HTML: rearrange drawer overlap order
    * CSS: add custom properties to allow padding changes to flow through
    * JS: add function to stretch drawer handle to drawer height

--- slim.js
+++ slim.js
@@ -1,12 +1,52 @@
-// $(function) same as $(document).ready(function)
+// ---------- FUNCTIONS --------------------------------------------------------
 
-$(function(){
+function handleLength() {
+  // listen for changes in height of public layer
+  // apply measurement to width of .handle
+  $(".handle").width($("#publayer").height());
+}
+function moveSettings() {
+  // switch applied classes
+  // case 1
   $(".bottom").click(function(){
     $("#settings, #publayer, #beacon, #itts, #pois, #wallet")
       .toggleClass("rad1 rad2");
   })
+  // case 2
   $(".top").click(function(){
     $("#settings, #publayer, #beacon, #itts, #pois, #wallet")
       .toggleClass("rad1 rad2");
   })
+}
+
+// ---------- WINDOW RESIZE EVENT ----------------------------------------------
+
+$(window).resize(function() {
+  // for resizes that are not resource-intensive
+  handleLength();
+})
+
+// for resizes that are somewhat resource-intensive
+var resizeTimer;
+$(window).resize(function() {
+  console.log(resizeTimer);
+  if (resizeTimer) { // null check
+    clearTimeout(resizeTimer);
+  }
+  resizeTimer = setTimeout(onWindowResize, 500);
+  // 500 (half a second) is arbitrary, can change
+  // (i.e. optimization opportunity)
+});
+// alternatives to consider: debouncing, throttling
+// especially when there are graphs to redraw
+function onWindowResize(){
+  // save for later
+}
+
+// ---------- DOCUMENT READY ---------------------------------------------------
+
+$(function(){
+  // $(function) same as $(document).ready(function)
+  handleLength();
+  moveSettings();
 })
