40a3c377e1e752c7f3848a3cacf21fd1cc279f89
Janning <thejanning@The.Purge>
2019-03-28 17:32:57 -0700

    Before Destroying Drawers
    * converted outside double-quotes to single-quotes for consistency and ease of adding quotes in long strings
    * converted border values to custom properties for precision and flexibility (and value passing)
    * converted panel width values to custom properties for flexibility (and value passing)
    * added hover effect as soft predecessor (reminder) to add tooltips

--- slim.js
+++ slim.js
@@ -1,25 +1,59 @@
-// ---------- FUNCTIONS --------------------------------------------------------
+// ========== FUNCTIONS ========================================================
 
 function handleLength() {
-  // listen for changes in height of public layer
-  // apply measurement to width of .handle
-  $(".handle").width($("#publayer").height());
+  // establish how long the handles to the drawers should be
+  $('.handle').outerWidth(
+    // measure height of public layer and height of its nav
+    $('#publayer').innerHeight() - $('#publayer nav').outerHeight()
+  ); // apply measurement to width of .handle
+  // $('.handle label').outerWidth($('.handle').outerWidth());
+  // console.log($('.handle label').innerHeight());
+  // console.log($('.handle').innerHeight());
+  // $('.handle label').innerHeight($('.handle').innerHeight());
 }
 function moveSettings() {
   // switch applied classes
   // case 1
-  $(".bottom").click(function(){
-    $("#settings, #publayer, #beacon, #itts, #pois, #wallet")
-      .toggleClass("rad1 rad2");
+  $('#settings .bottom').click(function(){
+    $('#settings, #publayer, #beacon, #itts, #pois, #wallet')
+      .toggleClass('togSettingsRad1 togSettingsRad2');
   })
   // case 2
-  $(".top").click(function(){
-    $("#settings, #publayer, #beacon, #itts, #pois, #wallet")
-      .toggleClass("rad1 rad2");
+  $('#settings .top').click(function(){
+    $('#settings, #publayer, #beacon, #itts, #pois, #wallet')
+      .toggleClass('togSettingsRad1 togSettingsRad2');
   })
 }
 
-// ---------- WINDOW RESIZE EVENT ----------------------------------------------
+function panelWidthPOIs() {
+  // switch applied classes
+  // case 1
+  $('#pois .close').click(function(){
+    $('#pois .panel')
+      .toggleClass('togPOIsRad1 togPOIsRad2');
+  })
+  // case 2
+  $('#pois .open').click(function(){
+    $('#pois .panel')
+      .toggleClass('togPOIsRad1 togPOIsRad2');
+  })
+}
+
+function panelWidthWallet() {
+  // switch applied classes
+  // case 1
+  $('#wallet .close').click(function(){
+    $('#wallet .panel')
+      .toggleClass('togWalletRad1 togWalletRad2');
+  })
+  // case 2
+  $('#wallet .open').click(function(){
+    $('#wallet .panel')
+      .toggleClass('togWalletRad1 togWalletRad2');
+  })
+}
+
+// ========== WINDOW RESIZE EVENT ==============================================
 
 $(window).resize(function() {
   // for resizes that are not resource-intensive
@@ -43,10 +77,12 @@ function onWindowResize(){
   // save for later
 }
 
-// ---------- DOCUMENT READY ---------------------------------------------------
+// ========== DOCUMENT READY ===================================================
 
 $(function(){
   // $(function) same as $(document).ready(function)
   handleLength();
   moveSettings();
+  panelWidthPOIs();
+  panelWidthWallet();
 })
